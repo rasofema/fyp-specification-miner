a02de50e63df73aad67f331e7a4dabbfad0c0181
Raquel <raquel.silva.20@ucl.ac.uk>
2023-12-06 00:59:39 +0000

    clean-up

--- src/main/java/org/example/IterOracle.java
+++ src/main/java/org/example/IterOracle.java
@@ -5,8 +5,7 @@ import de.learnlib.oracle.SingleQueryOracle.SingleQueryOracleDFA;
 import net.automatalib.word.Word;
 
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.*;
 
 enum Function {
     hasNextTrue,
@@ -15,33 +14,26 @@ enum Function {
 };
 
 public class IterOracle implements SingleQueryOracleDFA<Function> {
-    private ArrayList<Integer> bl;
-    private Iterator<Integer> iter;
+    private Iterator<Object> iter;
     public IterOracle() {
         reset();
     }
 
     private void reset() {
-        this.bl = new ArrayList<>();
-        iter = bl.iterator();
+        this.iter = Collections.emptyIterator();
     }
 
     private Boolean step(Function function) {
         switch (function) {
             case hasNextTrue -> {
                 if (!this.iter.hasNext()) {
-                    if (this.bl.isEmpty()) {
-                        this.bl.add(0);
-                    }
-                    this.iter = this.bl.iterator();
+                    this.iter = Collections.singletonList(null).iterator();
                     if (!this.iter.hasNext()) throw new RuntimeException("Iterator should have next");
                 }
             }
             case hasNextFalse -> {
-                if (this.iter.hasNext()) {
-                    while (this.iter.hasNext()) {
-                        this.iter.next();
-                    }
+                while (this.iter.hasNext()) {
+                    this.iter.next();
                 }
             }
             case next -> {
